f4a581e798f4518ac7e5dfe8e0c3f39baea76504
hxb-au <hxb.aus@gmail.com>
2016-03-03 00:22:37 +0930

    libGDX and Android Studio Setup

---
+++ desktop/src/au/id/hxb/cathedroid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package au.id.hxb.cathedroid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import au.id.hxb.cathedroid.CathedroidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CathedroidGame(), config);
+	}
+}
---
+++ html/src/au/id/hxb/cathedroid/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package au.id.hxb.cathedroid.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import au.id.hxb.cathedroid.CathedroidGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CathedroidGame();
+        }
+}
