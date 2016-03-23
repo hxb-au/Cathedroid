6648ecc16e0c19940f20fb4725d24b7d749e911e
hxb-au <hxb.aus@gmail.com>
2016-03-24 00:05:43 +0930

    Drag a Cathedral around
    Kinda works, but click is too sensitive and doesn't happen if it was
    dragged at all. Jen can't use it

--- core/src/au/id/hxb/cathedroid/screens/GameScreen.java
+++ core/src/au/id/hxb/cathedroid/screens/GameScreen.java
@@ -6,6 +6,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -15,51 +19,66 @@ import au.id.hxb.cathedroid.CathedroidGame;
  * Created by hxb on 6/03/2016.
  */
 public class GameScreen implements Screen {
-    CathedroidGame game;
-    SpriteBatch batch;
-    Texture placeholder;
-    OrthographicCamera cam;
-    GameInputListener gameInputListener;
+    private CathedroidGame game;
+    private OrthographicCamera cam;
+    private SpriteBatch batch;
     private int midPointX, midPointY;
     private Viewport viewport;
+    private Stage stage;
 
 
     public GameScreen(CathedroidGame game) {
         Gdx.app.log("GameScreen", "Attached");
-        batch = new SpriteBatch();
-        placeholder = new Texture(Gdx.files.internal("board_on_grey.png"));
         this.game = game;
         int nativeWidth = 1280;
         int nativeHeight = 720;
         midPointX = nativeWidth / 2;
         midPointY = nativeHeight / 2;
+
         cam = new OrthographicCamera(nativeWidth,nativeHeight);
         cam.setToOrtho(false, nativeWidth,nativeHeight);
         viewport = new FitViewport(nativeWidth, nativeHeight, cam);
-        gameInputListener = new GameInputListener(game, cam, this);
+        batch = new SpriteBatch();
+        stage = new Stage(viewport, batch);
+
+        Image bgImg = new Image(new Texture(Gdx.files.internal("board_on_grey.png")));
+        bgImg.setName("bg");
+        PieceActor cathedralPiece = new PieceActor(new Texture(Gdx.files.internal("cathedral_piece.png")),"Cathedral",
+                new Rectangle(0,100,150,50),new Rectangle(50,0,50,200),new Rectangle(50,0,50,200),75f,125f);
+
+        stage.addActor(bgImg);
+        stage.addActor(cathedralPiece);
+
+
+
 
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(gameInputListener);
+        Gdx.input.setInputProcessor(stage);
         viewport.apply();
-
-    }
+        }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0f, 1f, 0f, 1f); // Sets a Color to Fill the Screen with (RGB = 0, 0, 0), Opacity of 1 (100%)
+        Gdx.gl.glClearColor(.7f, .7f, .7f, 1f); // Sets a Color to Fill the Screen with (RGB = 0, 0, 0), Opacity of 1 (100%)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Fills the screen with the selected color
 
-        batch.setProjectionMatrix(viewport.getCamera().combined);
+        //do i still need this?
+        //batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        /*
         batch.begin();
         batch.disableBlending();
 
         batch.draw(placeholder,midPointX-placeholder.getWidth()/2,midPointY-placeholder.getHeight()/2);
 
         batch.enableBlending();
-        batch.end();
+        batch.end();*/
+
+        stage.act(delta);
+        stage.draw();
 
     }
 
@@ -68,6 +87,7 @@ public class GameScreen implements Screen {
         Gdx.app.log("GameScreen", "resizing");
         Gdx.app.log("Width", Integer.toString(width));
         Gdx.app.log("Height", Integer.toString(height));
+        //shoudl this be a reference via stage?
         viewport.update(width, height);
     }
 
---
+++ core/src/au/id/hxb/cathedroid/screens/PieceActor.java
@@ -0,0 +1,194 @@
+package au.id.hxb.cathedroid.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.actions.*;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
+
+/**
+ * Created by Hayden on 23/03/2016.
+ */
+public class PieceActor extends Image {
+    private Rectangle hitBox1, hitBox2, hitBox3;
+    private Vector2 v2before, v2after;
+    private float deltaTheta;
+
+
+
+    public PieceActor(Texture texture, String name,
+                      Rectangle hitBox1, Rectangle hitBox2, Rectangle hitBox3,
+                      float originX, float originY) {
+
+        super(texture);
+
+        this.hitBox1 = hitBox1;
+        this.hitBox2 = hitBox2;
+        this.hitBox3 = hitBox3;
+
+        this.setName(name);
+        this.setOrigin(originX, originY);
+        this.setTouchable(Touchable.enabled);
+
+        this.addListener(new PieceClickListener());
+        //this.addListener(new PieceDragListener());
+
+
+
+    }
+
+
+    @Override
+    public Actor hit(float x, float y, boolean touchable) {
+
+        // check touchable and override
+        if ( touchable && !this.isTouchable()) {
+            return null;
+        }
+
+        // check actual collisions with hitboxes
+        if (hitBox1.contains(x,y) || hitBox2.contains(x,y) || hitBox3.contains(x,y) ){
+                return this;
+        }
+
+        // nope out
+        return null;
+    }
+
+    /*class PieceDragListener extends DragListener {
+        @Override
+        public void drag(InputEvent event, float x, float y, int pointer) {
+            PieceActor.this.addAction(Actions.moveBy(x - dragStartX, y - dragStartY));
+        }
+
+        @Override
+        public void dragStart(InputEvent event, float x, float y, int pointer) {
+            dragStartX = x;
+            dragStartY = y;
+
+        }
+
+        @Override
+        public void dragStop(InputEvent event, float x, float y, int pointer) {
+            super.dragStop(event, x, y, pointer);
+        }
+
+    }*/
+
+    class PieceClickListener extends ClickListener {
+
+        private float dragStartX = 0, dragStartY = 0;
+        private float deltaX, deltaY;
+        private Vector2 tmpInV2 = new Vector2(), tmpOutV2 = new Vector2();
+        private boolean dragged = false;
+
+        public PieceClickListener() {
+            this.setTapSquareSize(5 * this.getTapSquareSize());
+        }
+
+        @Override
+        public void clicked(InputEvent event, float x, float y) {
+            PieceActor.this.setRotation(PieceActor.this.getRotation() - 90);
+            Gdx.app.log("Input", "Clicked");
+        }
+
+        @Override
+        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+            dragStartX = x;
+            dragStartY = y;
+            dragged = false;
+            return true;
+        }
+
+        @Override
+        public void touchDragged(InputEvent event, float x, float y, int pointer) {
+            tmpInV2.x = x - dragStartX;
+            tmpInV2.y = y - dragStartY;
+            tmpInV2.rotate(PieceActor.this.getRotation());
+            //PieceActor.this.addAction(Actions.moveBy(tmpOutV2.x, -tmpOutV2.y));
+            PieceActor.this.addAction(Actions.moveBy(tmpInV2.x, tmpInV2.y));
+            dragged = true;
+        }
+
+        @Override
+        public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            if (!dragged)
+                PieceActor.this.setRotation(PieceActor.this.getRotation() - 90);
+            //PieceActor.this.addAction(Actions.moveTo(0, 0 ));
+        }
+    }
+/*
+    class PieceInputListener extends InputListener {
+        @Override
+        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+            return true;
+        }
+
+        @Override
+        public void touchDragged(InputEvent event, float x, float y, int pointer) {
+            super.touchDragged(event, x, y, pointer);
+        }
+
+        @Override
+        public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            super.touchUp(event, x, y, pointer, button);
+        }
+
+        @Override
+        public boolean scrolled(InputEvent event, float x, float y, int amount) {
+            return super.scrolled(event, x, y, amount);
+        }
+    }
+*/
+    /*class PieceInputListener extends ActorGestureListener {
+
+        @Override
+        public void pinch(InputEvent event,
+                          Vector2 initialPointer1, Vector2 initialPointer2,
+                          Vector2 pointer1,        Vector2 pointer2) {
+
+            v2before = initialPointer2.sub(initialPointer1);
+            v2after = pointer2.sub(pointer1);
+            deltaTheta = v2after.angle() - v2before.angle();
+
+            if (deltaTheta > 10){
+                PieceActor.this.setRotation(PieceActor.this.getRotation() + 90);
+
+            }
+            if (deltaTheta < -10){
+                PieceActor.this.setRotation(PieceActor.this.getRotation() - 90);
+
+            }
+        }
+
+        @Override
+        public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
+
+            if (PieceActor.this.hit(x,y,true) != null) {
+                PieceActor.this.addAction(Actions.moveTo(x, y));
+
+            }
+        }
+
+        @Override
+        public boolean longPress(Actor actor, float x, float y) {
+            PieceActor.this.setRotation(PieceActor.this.getRotation() + 90);
+            return true;
+        }
+    }*/
+}
